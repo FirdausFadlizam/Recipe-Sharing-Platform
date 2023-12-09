ef7691b63fb303e645d3f55a0b3c3e21235d1659
Jaishree Jaikumar <jaishree0638@gmail.com>
2023-12-09 08:26:51 -0500

    code cleanup

--- models/loginModel.js
+++ models/loginModel.js
@@ -24,7 +24,7 @@ const userSchema = mongoose.Schema(
 
     {
         timestamps: true
-    });//end of the Product Schema prototype/object
+    });//end of the User Schema prototype
 
     const User = mongoose.model('User', userSchema);
 
--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.post('/user', async (req, res) => {
     }
 });
 
+//get data
 app.get('/login', async (req, res) => {
     try {
         const { username, password } = req.query;
@@ -49,65 +50,3 @@ app.get('/login', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
-
-//get users data
-/*app.get('/users', async (req, res) => {
-    try {
-        const user = await User.find({});
-        res.status(200).json(user);
-    }
-    catch(error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-//get a single product
-app.get('/users/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-        const user = await User.findById(id);
-        res.status(200).json(user);
-    }
-    catch(error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-//modify data
-/*app.put('/users/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-        const user = await User.findByIdAndUpdate(id, req.body);
-
-        if(!user) {
-            return res.status(404).json(
-                {message: `cannot find user with id ${id}`}
-                );
-        }//end if
-
-        const updatedUser = await User.findById(id);
-
-        res.status(200).json(updatedUser);
-    }
-    catch(error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-
-
-app.delete('/users/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-        const user = await User.findByIdAndDelete(id);
-
-        if(!user) {
-            return res.status(404).json({message: `cannot find user with id ${id}`});
-        }
-
-        res.status(200).json(user);
-    }
-    catch(error) {
-        res.status(500).json({message: error.message});
-    }
-});*/
