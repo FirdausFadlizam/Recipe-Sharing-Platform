327a03d5dd86882a1c1946e1f35444ab91d351dc
Muhammad Adib Bin Halizam <adibhali@umich.edu>
2023-12-09 23:09:33 -0500

    latest

--- server.js
+++ server.js
@@ -55,6 +55,37 @@ app.get('/recipes', async (req, res) => {
     }
 });
 
+app.get('/recipes/:id', async (req, res) => {
+    try {
+        // `req.params.id` contains the id from the URL
+        const recipe = await Recipe.findById(req.params.id);
+
+        // If recipe doesn't exist, send a 404 error
+        if (!recipe) {
+            return res.status(404).json({ message: 'No such recipe found' });
+        }
+
+        // Respond with the recipe
+        res.json(recipe);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+//post data
+app.post("/createRecipe", async (req, res) => {
+    try {
+        const recipe = await Recipe.create(req.body);
+
+        res.status(200).json(recipe);
+    }
+    catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+});
+
 
 app.delete("/deleteRecipe/:id", async (req, res) => {
     try {
